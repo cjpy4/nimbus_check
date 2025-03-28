7eaa234edb48040514a5b1cbf3c6ccbe99dca80c
jpye4 <jimmycliff4@gmail.com>
2025-03-28 01:23:12 -0500

    fixed hono function

--- apps/tech-check-worker/src/index.ts
+++ apps/tech-check-worker/src/index.ts
@@ -2,13 +2,13 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
+  
+  const req = new Request('Hello!', {
+    method: 'POST',
+  })
+  const res = await app.request(req)
 
-    const req = new Request('Hello!', {
-      method: 'POST',
-    })
-    const res = await app.request(req)
-    expect(res.status).toBe(201)
   return c.text('Hello Hono!')
 })
 
