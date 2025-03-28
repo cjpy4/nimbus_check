8560ef3f9d319fd01934f2d4b1322ffe457ec3bc
jpye4 <jimmycliff4@gmail.com>
2025-03-28 16:04:20 -0500

    added worker routing and some test requests

--- apps/tech-check-worker/src/index.ts
+++ apps/tech-check-worker/src/index.ts
@@ -1,15 +1,37 @@
+import { env } from 'cloudflare:workers'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', async (c) => {
-  
-  const req = new Request('https://sickw.com/api.php?format=beta&key=V29-1J2-0JX-CDL-DFT-TUZ-SM6-BHJ&imei=354442067957452&service=demo', {
-    method: 'GET',
-  })
-  const res = await app.request(req)
+app.get('/', (c) => {
+  return c.text('Hello from Tech Check Worker!')
+})
 
-  return c.text(await res.text())
+app.get('/stdcheck', async (c) => {
+
+  let url = ''
+
+  try {
+    const api_key = env.SICKW_API_KEY
+    url = `https://sickw.com/api.php?format=beta&key=${api_key}&imei=354442067957452&service=demo`
+    
+  } catch (error) {
+    return c.json({ error: 'Failed to fetch API key' }, 500)
+  }
+
+    try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        // Add any required headers
+      }
+    })
+    const data = await response.json()
+    return c.json(data)
+  } catch (error) {
+    return c.json({ error: 'Failed to fetch data' }, 500)
+  }
 })
 
 export default app
