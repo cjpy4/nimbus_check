2bedda6acbaa5ed38002fecd6328be95b7b6d227
jpye4 <jimmycliff4@gmail.com>
2025-03-28 01:33:59 -0500

    adding starter api request in worker

--- apps/tech-check-worker/src/index.ts
+++ apps/tech-check-worker/src/index.ts
@@ -4,12 +4,12 @@ const app = new Hono()
 
 app.get('/', async (c) => {
   
-  const req = new Request('Hello!', {
-    method: 'POST',
+  const req = new Request('https://sickw.com/api.php?format=beta&key=V29-1J2-0JX-CDL-DFT-TUZ-SM6-BHJ&imei=354442067957452&service=demo', {
+    method: 'GET',
   })
   const res = await app.request(req)
 
-  return c.text('Hello Hono!')
+  return c.text(await res.text())
 })
 
 export default app
